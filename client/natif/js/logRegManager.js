b0b35a76ce97de07b416e217ed24ffea0515a1bc
Blanck <alexandre.elisab8@gmail.com>
2021-02-20 13:43:55 +0100

    Add login and register
    Ajout des pages login et register

---
+++ client/natif/js/logRegManager.js
@@ -0,0 +1,10 @@
+var login = document.getElementById("log_btn");
+var register = document.getElementById("reg_btn");
+
+login.addEventListener('click', function() {
+    window.location.href = "pages/login.html";
+});
+
+register.addEventListener('click', function() {
+    window.location.href = "pages/register.html";
+});
